91390a85016b016d69f36f38234c22beaaeb8a07
Sariq Shaikh <shaikh.mshariq@gmail.com>
2012-10-30 23:19:38 +0530

    Initial check in of Antecedent

---
+++ src/com/sitegraph/core/util/WebAppUtils.java
@@ -0,0 +1,67 @@
+/**
+ * This class will hold utility methods to be used in web layer.
+ */
+package com.sitegraph.core.util;
+
+import java.io.File;
+import java.util.UUID;
+
+import com.sitegraph.core.attributes.ImageAttributes;
+import com.sitegraph.core.attributes.pdf.PdfAttributes;
+
+public class WebAppUtils {
+
+	/*
+	 * This method returns Unique GUID for each input URL String
+	 */
+	public static String getUniquePath(String inputString){
+		return UUID.nameUUIDFromBytes(inputString.getBytes()).toString()+"/";
+	}
+	
+	/*
+	 * This method is used to resolve image name based on its attributes.
+	 */
+	public static String resolveImageStoragePath(ImageAttributes imageAttributes,String inputUrl){
+		String imageFilepath = WebAppConstants.IMAGE_ABSOLUTE_PATH+ getUniquePath(inputUrl);
+		File domainDir = new File(imageFilepath);
+		domainDir.mkdir();
+		return  imageFilepath + WebAppConstants.DEFAULT_IMAGE_PREFIX + imageAttributes.getImageWidth() + "x" + imageAttributes.getImageHeight() + imageAttributes.getImageSuffix();
+	}
+	/*
+	 * This method is used to resolve image relative path of server.
+	 */
+	public static String resolveImageWebPath(ImageAttributes imageAttributes,String inputUrl){
+		return "/"+ WebAppConstants.DEFAULT_IMAGE_ABSOLUTE_PATH + getUniquePath(inputUrl) + WebAppConstants.DEFAULT_IMAGE_PREFIX + imageAttributes.getImageWidth() + "x" + imageAttributes.getImageHeight() + imageAttributes.getImageSuffix();
+	}
+	/*
+	 * This method is used to resolve image name based on its attributes.
+	 */
+	public static String resolvePdfStoragePath(PdfAttributes pdfAttributes,String inputUrl){
+		String pdfFilepath =  getUniquePath(inputUrl);
+		File domainDir = new File(pdfFilepath);
+		domainDir.mkdir();
+		return  pdfFilepath + WebAppConstants.DEFAULT_PDF_PREFIX + pdfAttributes.getPdfTemplateSize()+SiteGraphConstants.PDF_FILE_SUFFIX;
+	}
+	/*
+	 * This method is used to resolve image relative path of server.
+	 */
+	public static String resolvePdfWebPath(PdfAttributes pdfAttributes,String inputUrl){
+		return "/"+ WebAppConstants.DEFAULT_PDF_ABSOLUTE_PATH + getUniquePath(inputUrl) + WebAppConstants.DEFAULT_PDF_PREFIX + pdfAttributes.getPdfTemplateSize() + SiteGraphConstants.PDF_FILE_SUFFIX;
+	}
+	/*
+	 * This method is used to validate Image Type
+	 */
+	public static String validateImageType(String imageSuffix){
+		String result = "";
+		String imageType= ".";
+		imageType += imageSuffix;
+		if(SiteGraphConstants.PNG_IMAGE_SUFFIX.equals(imageType))
+			result = SiteGraphConstants.PNG_IMAGE_SUFFIX;
+		else if(SiteGraphConstants.JPEG_IMAGE_SUFFIX.equals(imageType))
+			result = SiteGraphConstants.JPEG_IMAGE_SUFFIX;
+		else
+			result = SiteGraphConstants.PNG_IMAGE_SUFFIX;
+		return result;
+		
+	} 
+}
---
+++ src/com/sitegraph/web/controller/PdfController.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package com.sitegraph.web.controller;
+
+import java.io.File;
+import java.rmi.RemoteException;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.ServletContextAware;
+
+import com.sitegraph.core.attributes.pdf.PdfAttributes;
+import com.sitegraph.core.pdf.IPdfThumbnailer;
+import com.sitegraph.core.util.WebAppConstants;
+import com.sitegraph.core.util.WebAppUtils;
+
+
+@RequestMapping("/pdf")
+public class PdfController implements ServletContextAware{
+
+	@Autowired()
+	private IPdfThumbnailer thumbnailer;
+	private ServletContext servletContext;
+	
+	/**
+	 * Generates image using default PNG image type and size.
+	 * @param url URL of web page.
+	 * @return
+	 * @throws RemoteException 
+	 */
+	@RequestMapping(method=RequestMethod.GET, value="/makepdf/")
+	public String makePdf(@RequestParam("URL") String url,@RequestParam(value="latest",required=false,defaultValue="false") boolean latest) throws RemoteException{
+		String response = "redirect:/";
+		PdfAttributes pdfAttribute = new PdfAttributes();
+		pdfAttribute.setUrl(url);
+		pdfAttribute.setPdfPath(WebAppUtils.resolvePdfStoragePath(pdfAttribute, url));
+		if(!latest && new File(pdfAttribute.getPdfPath()).exists()){
+			String generatedImage =  WebAppUtils.resolvePdfWebPath(pdfAttribute, pdfAttribute.getUrl().toString());
+			response = "redirect:/"+generatedImage+"?rand="+new Date().getTime();
+		}else{
+				if(thumbnailer.makePdfFromUrl(pdfAttribute))
+				{
+					String generatedImage =  WebAppUtils.resolvePdfWebPath(pdfAttribute, pdfAttribute.getUrl().toString());
+					response = "redirect:/"+generatedImage+"?rand="+new Date().getTime();
+				}
+				
+		}
+		return response;
+	}
+	
+	/**
+	 * Generates image using default PNG image type and size.
+	 * @param url URL of web page.
+	 * @return
+	 * @throws RemoteException 
+	 */
+	@RequestMapping(method=RequestMethod.POST, value="/makepdf/")
+	public String makePdf(@RequestParam("HTML") String html) throws RemoteException{
+		long handler = new Date().getTime();
+		PdfAttributes pdfAttribute = new PdfAttributes();
+		pdfAttribute.setPdfPath(WebAppUtils.resolvePdfStoragePath(pdfAttribute, html));
+		pdfAttribute.setHtml(html);
+		String response = "redirect:/";
+		
+		
+		if(thumbnailer.makePdfFromHTML(pdfAttribute,WebAppUtils.getUniquePath(pdfAttribute.getHtml())))
+		{
+			String generatedImage =  WebAppUtils.resolvePdfWebPath(pdfAttribute, String.valueOf(handler));
+			response = "redirect:/"+generatedImage+"?rand="+handler;
+		}	
+		
+		return response;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.context.ServletContextAware#setServletContext(javax.servlet.ServletContext)
+	 */
+	
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+		WebAppConstants.PDF_ABSOLUTE_PATH = this.servletContext.getRealPath("/") + WebAppConstants.PDF_ABSOLUTE_PATH; 
+		System.out.println("Default path : "+ WebAppConstants.PDF_ABSOLUTE_PATH);
+	} 
+	
+	/**
+	 * @return
+	 */
+	public ServletContext getServletContext(){
+		return this.servletContext;
+	} 
+}
